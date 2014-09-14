ac9469ca223ed45a16ce619a2a9001abc85ba24d
Matt Beaudry <beaudrymatt@gmail.com>
2014-09-14 03:14:42 -0400

    styles sign reading, fixed enter and space bar bugs

--- js/beaudryland.js
+++ js/beaudryland.js
@@ -801,7 +801,53 @@ placeSign = function(objectid, block) {
 };
 readSign = function(block) {
 	var message = $('.block:eq('+block+')').attr("data-text");
-	alert(message);
+	//alert(message);
+	var html = '<div class="bubble-wrap">';
+				html += '<div class="bubble-link">';
+		  			html += '<form class="bubble-form" action="#">';
+		  				html += '<h3>'+message+'</h3>';
+		    			//html += '<input class="bubble-input" type="text" placeholder="Text">';
+		    			//html += '<textarea class="bubble-text bubble-input" rows="2" cols="30" placeholder="type message"></textarea>';
+		    			html += '<input type="submit" value="Okay!" >';
+		  			html += '</form>';
+		  		html += '</div>';
+		  	html += '<span class="bubble-hangdown-1"></span>';
+		  	html += '<span class="bubble-hangdown-2"></span>';
+		  	html += '<span class="bubble-hangdown-3"></span>';
+		  	html += '<span class="bubble-hangdown-4"></span>';
+		html += '</div>';
+
+	$('.objectid-'+1).append(html);
+
+	$('.bubble-form').submit(function(e) {
+
+		//var message = $('.bubble-text').val();
+
+		//find block that the player is facing
+		var id = 1;
+		var direction = getObjectDirection(id, "player");
+		var block = getObjectCurrentBlock(id);
+		switch (direction) {
+			case "up": block = block - (mapwidth+1); break;
+			case "down": block = block + (mapwidth-1); break;
+			case "left": block = block - 2; break;
+			case "right": block = block; break;
+		}
+		
+		$('.block:eq('+block+')').attr("data-text", message);
+		//$('.block:eq('+block+')').remove();
+
+		//console.log('write message to block #: ' + block);
+		//console.log('write message: ' + message);
+
+		$('.bubble-wrap').remove();
+
+		event.preventDefault();
+
+	});
+
+
+	
 };
 
 
