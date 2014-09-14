34712d144f8c651bd837bea77048936645064c5d
Matt Beaudry <beaudrymatt@gmail.com>
2014-09-14 12:42:28 -0400

    new version of jquery, minor fixes

--- beaudryland.php
+++ beaudryland.php
@@ -303,6 +303,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
 		        		<img src="img/item-spear.png" title="Spear">
 		        		<ul>
 		        			<li>can be thrown</li>
+		        			<li>selecting the spear with spawn an animal</li>
 		        		</ul>
 		        	</li>
 		        	<li>
@@ -439,7 +440,7 @@ if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
         	?>
         </div>
 
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.0.0.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.1.1.js"><\/script>')</script>
         <script src="js/vendor/jquery.color.js"></script>
         <script src="js/vendor/timbre.js"></script>
         <script src="js/plugins.js"></script>
--- index.php
+++ index.php
@@ -180,7 +180,7 @@ if (isset($_GET['error'])){ $error = $_GET['error']; }else{ $error = ""; }
         <!--
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
         -->
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.0.0.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.1.1.js"><\/script>')</script>
         <!-- <script src="js/vendor/jquery.color.js"></script> -->
         <script src="js/vendor/jquery.validate.js"></script>
         <!-- <script src="js/vendor/timbre.js"></script> -->
