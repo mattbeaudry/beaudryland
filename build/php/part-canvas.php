97ac9238e9298acd39fd23eb7977a1893ca57d90
Matt Beaudry <mattb@surveymonkey.com>
2018-03-01 02:01:04 -0500

    item builder animated SVGs! with db saving

---
+++ build/php/part-canvas.php
@@ -0,0 +1,31 @@
+<div class="the-fucking-canvas clearfix">
+       <div class="canvas-pixel" data-pixel="1" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="2" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="3" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="4" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="5" data-color="transparent"></div>
+
+       <div class="canvas-pixel" data-pixel="6" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="7" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="8" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="9" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="10" data-color="transparent"></div>
+
+       <div class="canvas-pixel" data-pixel="11" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="12" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="13" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="14" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="15" data-color="transparent"></div>
+
+       <div class="canvas-pixel" data-pixel="16" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="17" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="18" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="19" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="20" data-color="transparent"></div>
+
+       <div class="canvas-pixel" data-pixel="21" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="22" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="23" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="24" data-color="transparent"></div>
+       <div class="canvas-pixel" data-pixel="25" data-color="transparent"></div>
+</div>
